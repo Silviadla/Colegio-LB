785d43b052fa44d61e74d612d05601b7924f401d
Silvia <silviadelucasalv@gmail.com>
2023-01-31 17:50:16 +0100

    Commit 1

---
+++ Materia.java
@@ -0,0 +1,11 @@
+public class Materia{
+    private long idMateria;
+    private String nombreMat;
+    /**
+     * Constructor for objects of class Materia
+     */
+    public Materia(long idMateria, String nombreMat){
+        this.idMateria = idMateria ;
+        this.nombreMat = nombreMat;
+    }
+}
