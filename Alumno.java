a5279e8a45ea8f77a04bbd1c6dffd7c6d8c6b555
Silvia <silviadelucasalv@gmail.com>
2023-01-31 18:58:30 +0100

    Commit 5

--- Alumno.java
+++ Alumno.java
@@ -1,7 +1,12 @@
-
-public class Alumno
-{
+public class Alumno{
     private int idAlumno;
     private String nombre;
     private Materia asignatura;
+    /**
+     * Constructor for objects of class Materia
+     */
+    public Alumno(int idAlumno, String nombre){
+        this.idAlumno = idAlumno;
+        this.nombre = nombre;
+    }
 }
